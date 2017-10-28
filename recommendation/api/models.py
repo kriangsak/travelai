d0c950239b3e6b76f827b3b4d37bd1e2132b188f
Kriang <kriangsak.loisaeng@gmail.com>
2017-10-28 20:51:22 +0700

    Change folder name

--- recommendation/api/models.py
+++ recommendation/api/models.py
@@ -1,14 +1,3 @@
 from django.db import models
-import pandas as pd
-import numpy as np
 
 # Create your models here.
-
-class CuisineSuggester():
-    @staticmethod
-    def suggest():
-
-        frame = pd.read_csv('csv/rating_final.csv')
-        cuisine = pd.read_csv('csv/chefmozcuisine.csv')
-
-        return list(np.arange(0,10))
--- recommendation/api/views.py
+++ recommendation/api/views.py
@@ -1,34 +1,13 @@
-from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import UserSerializer, GroupSerializer
 
 from django.utils.encoding import force_text
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-import json
-from .models import CuisineSuggester
-
-class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
-class CuisinePopularity(APIView):
+class Hello(APIView):
     def get(self, request):
-        cuisine_list = CuisineSuggester().suggest()
-        return Response({
-            "suggested_cuisine": cuisine_list
-        })
+        return Response({"hello": "world"})
+    
+
+# your ml class here
